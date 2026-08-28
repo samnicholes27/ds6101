f2980107770140300a5b8a5da785d2c4988e5bc3
samnicholes27 <sam.nicholes27@gmail.com>
2026-08-28 17:55:03 -0400

    Reading update 20260827

--- 20260831_lecture2/20260831_script_reading.R
+++ 20260831_lecture2/20260831_script_reading.R
@@ -9,7 +9,7 @@ length(salary)
 
 plot(salary)
 
-# Measures of Center:
+# MEASURES OF CENTER -----------------------------------------------------------
 # mean > median - right skewed distribution
 mean(salary)
 median(salary)
@@ -27,15 +27,61 @@ abline(v = median(salary),
        lwd = 3, lty = 2)
 
 sum(salary < mean(salary))
-# salary < mean(salary) results in logical vector: TRUE | FALSE
+# salary <- mean(salary) results in logical vector: TRUE | FALSE
+# sum() coerces to 1s | 0s and adds the values resulting in the count of 1s
 
 length(salary < mean(salary)) # gives the length of the logical vector
-mean(salary < mean(salary)) 
+
+mean(salary < mean(salary))
+# mean() coerces boolean values to binary, 1 | 0
+# 1s add up to 145, which gives 0.60 when divided by the total 
+# and gives the percentage of 1s in the logical vector
 
 
 
+# MEASURES OF SPREAD -----------------------------------------------------------
 
-# sum(), mean() coerces logical values into 1 | 0 
-# sum() results in summation of binary values
-#
+var(salary) # not intuitive way to measure spread of data
+sd(salary) # more intuitive/interpretable way to measure spread of data
+mean(salary) + sd(salary) # $110,978.70 is 1SD away from the mean
+
+
+# Exercise: the correction, by hand
+# The vector `s` holds five values. R's var(s) is 16 (sd = 4).
+# Compute the OTHER version: the average squared deviation from the mean,
+# dividing by n rather than n - 1. Write it out; do not use var().
+n <- 5
+s <- rnorm(5, mean = 0, sd = 4)
+
+# Scenario A: s is a sample from population, p
+var_s <- sum((s - mean(s))^2) / (n - 1)
+var_s
+
+# Scenario B: s is the entire known population
+var_s <- sum((s - mean(s))^2) / (n)
+var_s
+
+
+# Exercise: below average
+# `salary` holds the 240 alumni salaries.
+# Return the PROPORTION of alumni who earn less than the mean salary
+# (a single number between 0 and 1).
+salary <- round(rlnorm(240, meanlog = 11, sdlog = 0.55)) 
+mean_salary <- mean(salary)
+alumni <- mean(salary < mean(salary))
+alumni
+
+
+# Interquartile Range (IQR)
+quantile(salary,
+         probs = c(0.05, 0.25, 0.5, 0.75, 0.95))
+
+# IQR/SD gives an idea of how data are spread out from the center
+# IQR/SD = 1.35 - normal distribution
+# IQR/SD < 1.35 - heavy tails (outliers, bulk of data is less centralized)
+# IQR/SD > SD - lighter tails (data is more heavily clustered around the center)
+IQR(salary)/sd(salary)
+# 1.25
+# 1.25 < 1.35 - heavy tailed distribution
+
 
