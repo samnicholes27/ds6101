f0cbbe8b4dd30969e589e3481d97041285a3f761
samnicholes27 <sam.nicholes27@gmail.com>
2026-09-09 18:36:57 -0400

    Lecture 3 reading updates

---
+++ 20260914_lecture3/20260914_lecture3_reading.R
@@ -0,0 +1,50 @@
+
+
+# PROBABILITY RULES ------------------------------------------------------------
+
+# P(A ∩ B) = P(B) * P(A|B), Multiplication Rule
+# Where A and B are both true simultaneously
+
+# P(A|B) = P(A ∩ B) / P(B)
+# Where P(B) > 0
+
+
+# P(A|B) = [P(B|A) * P(A)] / P(B)
+# Baye's Rule allows use to calculate P(B|A) or P(A|B),
+# depending on what we are initially given or know
+
+
+# EXAMPLE ----------------------------------------------------------------------
+# A disease affects 0.5% of the population; P(D) = 0.5
+# Total population is 1,000
+# Sensitivity = 99%; Out of the truly diseased, how many correctly test positive?
+#   P(+|D)
+
+# Specificity = 95%; Out of the NON-diseased, how many correctly test negative?
+#   P(-|ND)
+
+# Has the disease, "D"
+# Disease free, "ND"
+# Tested positive for the disease, "+"
+# Tested negative, "-"
+
+# QUESTION: You tested positive, but have no symptoms. What is the probability
+# that you have the disease?
+#     P(D|+), probability of disease given a positive test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
